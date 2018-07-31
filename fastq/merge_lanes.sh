58f5a75a25a7806837f61422d34084ecb9c19c7a
Xin Li <xli6@smsx10srw-srcf-d15-36.scg.stanford.edu>
2018-07-30 22:17:18 -0700

    Mon Jul 30 22:17:18 PDT 2018

--- fastq/merge_lanes.sh
+++ fastq/merge_lanes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fastq_dir=~/projects/kp1/fastq/output
-merged_fastq_dir=~/projects/kp1/fastq/output_merged
+fastq_dir=~/projects/motrpac/fastq/output
+merged_fastq_dir=~/projects/motrpac/fastq/output/merged
 
 #suffix of input fastq files
 SUF_R1=_R1_001.fastq.gz
@@ -12,12 +12,13 @@ OUT2_SUF=merge_R2.fastq.gz
 
 # If array is present, it is cleared, and then the zeroth element of array is set to the entire portion of string matched by regexp. If regexp contains parentheses, the integer-indexed elements of array are set to contain the portion of string matching the corresponding parenthesized subexpression. 
 
-indiv=$(ls -1 ${fastq_dir}/RD*_001.fastq.gz | awk '{ match($0, /(RD[0-9]+)_/, arr); print arr[1]}' | sort | uniq)
+# indiv=$(ls -1 ${fastq_dir}/RD*_001.fastq.gz | awk '{ match($0, /(RD[0-9]+)_/, arr); print arr[1]}' | sort | uniq)
+indiv=$(ls -1 ${fastq_dir}/*_001.fastq.gz | awk '{ match($0, /([\-A-z0-9]+)_L[0-9]+/, arr); print arr[1]}' | sort | uniq)
 
 for i in $indiv
 do
 	echo $i
-	zcat $FASTQ_DIR/${i}_*L00*${SUF_R1} | gzip >${merged_fastq_dir}/${i}_${OUT1_SUF} 
-	zcat $FASTQ_DIR/${i}_*L00*${SUF_R2} | gzip >${merged_fastq_dir}/${i}_${OUT2_SUF}
+	zcat ${fastq_dir}/${i}_*L00*${SUF_R1} | gzip >${merged_fastq_dir}/${i}_${OUT1_SUF} 
+	zcat ${fastq_dir}/${i}_*L00*${SUF_R2} | gzip >${merged_fastq_dir}/${i}_${OUT2_SUF}
 done
 
